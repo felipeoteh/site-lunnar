bdd09d9f46c19388bb6982c7dd9858bd215ef739
felipeoteh <felipe@agenciasimetria.com.br>
2022-04-06 07:53:42 -0300

    setup initial project

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  mode: "jit",
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "primary": "#AD9E57",
+        "secundary": "#414141",
+      }
+    },
+  },
+  plugins: [],
+}
